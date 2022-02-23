90bcb4f2bb733aa4a331fba1c6aa2ba70fce6b5e
gigu007 <gideonukaegbu007@gmail.com>
2022-02-23 15:56:57 +0100

    new commit

---
+++ js/bubblesort.js
@@ -0,0 +1,13 @@
+function bubblesort(arr){
+    for(var i=0;i<arr.length;i++){
+        for(var j=0;j<arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                var temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubblesort([23,12,34,14,56,38]));
