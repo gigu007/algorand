235f724c6e2e3abcad4a640f5acf6f92ad0d53d7
gigu007 <gideonukaegbu007@gmail.com>
2022-02-28 16:03:05 +0100

    another commit

--- js/singlyLinkedList.js
+++ js/singlyLinkedList.js
@@ -19,11 +19,30 @@ class singlyLinkedList{
         else{
             this.tail.next=newNode;
             this.tail=newNode;
-            this.length++;
+           
+            
         }
+        this.length++;
+        return this;
+    }
+    unshift(val){
+        let newNode=new Node(val);
+        if(!this.head){
+            this.head=newNode;
+            this.tail=newNode;
+        }
+        else{
+            newNode.next=this.head;
+            this.head=newNode
+        }
+        this.length++;
+        return this;
     }
 }
 let list=new singlyLinkedList();
 list.push('the first');
 list.push('second');
+list.push('third');
+list.unshift('zero');
+list.push('mnizero');
 console.log(list);
