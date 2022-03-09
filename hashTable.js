7b84d1849dc1879ca55da24d2ed4c723a1e31fc1
gigu007 <gideonukaegbu007@gmail.com>
2022-03-09 20:45:03 +0100

    initial commit

---
+++ hashTable.js
@@ -0,0 +1,51 @@
+ class hashTable{
+     constructor(size=53){
+         this.keyMap=new Array(size);
+
+     }
+     _hash(key){
+         let total=0;
+         let weird_prime=31;
+         for(let i=0;i<Math.min(key.length,100);i++){
+             let char =key[i];
+             let value=char.charCodeAt(0)-96;
+             total=(total*weird_prime+value)%this.keyMap.length;
+         }
+         return total;
+     }
+     set(key,value){
+         let index=this._hash(key);
+         if(!this.keyMap[index]){
+             this.keyMap[index]=[];
+         }
+         this.keyMap[index].push([key,value]);
+     }
+     get(key){
+         let index=this._hash(key);
+         if(this.keyMap[index]){
+             for(let i=0;i<this.keyMap[index].length;i++){
+                 if(this.keyMap[index][i][0]===key){
+                     return this.keyMap[index][i];
+                 }
+             }
+         }
+         return undefined;
+     }
+     getKeys(){
+        let keyArr=[];
+        for(let i=0;i<this.keyMap.length;i++){
+            if(this.keyMap[i]){
+                for(let j=0;j<this.keyMap[i].length;j++){
+                    keyArr.push(this.keyMap[i][j][1]);
+                }
+            }
+        
+        }
+        return keyArr;
+     }
+ }
+ let ht=new hashTable();
+ ht.set('green','first');
+ ht.set('yellow','second');
+ ht.set('blue','third');
+ht.getKeys();
--- js/test.js
+++ js/test.js
@@ -45,3 +45,51 @@ let plusFive=(number)=>{
 }
 let f=plusFive;
 console.log(f(15));
+
+
+const isEven = (n) => {
+    return n % 2 == 0;
+  }
+   
+  let printMsg = (evenFunc, num) => {
+    const isNumEven = evenFunc(num);
+    console.log(`The number ${num} is an even number: ${isNumEven}.`)
+  }
+   
+  // Pass in isEven as the callback function
+  printMsg(isEven, 4); 
+  // Prints: The number 4 is an even number: True.
+  //Assign a function to a variable originalFunc
+const originalFunc = (num) => { return num + 2 };
+ 
+//Re-assign the function to a new variable newFunc
+const newFunc = originalFunc;
+ 
+//Access the function's name property
+newFunc.name; //'originalFunc'
+ 
+//Return the function's body as a string
+newFunc.toString(); //'(num) => { return num + 2 }'
+ 
+//Add our own isMathFunction property to the function
+newFunc.isMathFunction = true;
+ 
+//Pass the function as an argument
+const functionNameLength = (func) => { return func.name.length }; 
+functionNameLength(originalFunc); //12
+ 
+//Return the function
+const returnFunc = () => { return newFunc };
+returnFunc(); //[Function: originalFunc]
+const numbers=[22,34,56,78,23,16,34,16];
+numbers.forEach(number=>{
+   console.log(number);
+})
+const finalParticipants=['mike','john','younis','arteta'];
+const announcements=finalParticipants.map(member=>{
+    return member + ' joined the contest';
+})
+// const announcements=finalParticipants.forEach(member=>{
+//     console.log( member + ' joined the contest');
+// })
+console.log(announcements);
